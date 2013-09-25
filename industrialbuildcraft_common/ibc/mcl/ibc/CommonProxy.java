1d94fc035423791ae390c5e30ba19c63b9867c4e
minecraftl539 <minecraftl539@users.noreply.github.com>
2013-09-25 16:09:21 +0200

    Update CommonProxy.java

--- industrialbuildcraft_common/ibc/mcl/ibc/CommonProxy.java
+++ industrialbuildcraft_common/ibc/mcl/ibc/CommonProxy.java
@@ -3,7 +3,7 @@ package ibc.mcl.ibc;
 public class CommonProxy {
     
     
-    // Client stuff
+    // Client stuff. NOTE: No renderers need to be here! this is because this is the server side proxy!
     public void registerRenderers() {
         
     }
